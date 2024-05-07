28d40e5dbffcd39543b60d7cbecb87a88d83c252
Bulbul Ahmed <bulbulahmed97811@gmail.com>
2024-05-06 23:45:04 -0700

    changed

--- src/RangeInput.java
+++ src/RangeInput.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class RangeInput {
+public class RangeInput {
+    private int maxTemperature;
+    private int minTmperature;
+    private int currentTemperature;
+
+    public RangeInput(){
+        minTmperature = 60;
+        maxTemperature = 80;
+        calculateTemperature();
+    }
+    private RangeInput(int min,int max){
+        minTmperature = min;
+        maxTemperature = max;
+        calculateTemperature();
+    }
+    public void calculateTemperature(){
+        currentTemperature = (minTmperature + maxTemperature)/2;
+    }
+    public void up(){
+        currentTemperature = Math.min(currentTemperature + 1,maxTemperature);
+    }
+    public void down(){
+        currentTemperature = Math.max(currentTemperature - 1,minTmperature);
+    }
+    public int getCurrentTemperature(){
+        return currentTemperature;
+    }
 }
+
